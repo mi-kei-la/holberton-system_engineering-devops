9dd3d40fec077326a051ad965507daec496e02e9
mi-kei-la <micaela.percovich@gmail.com>
2021-05-03 21:33:12 +0000

    requirement check

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,45 +2,45 @@
 """ This module makes a request to a RESTful API
 and exports the pertinent data to a CSV file.
 """
-import requests
-import csv
 from sys import argv
+import csv
+import requests
 
+if __name__ == "__main__":
+    if len(argv) < 2:
+        exit
 
-if len(argv) < 2:
-    exit
+    # Get employee name
+    emp_id = int(argv[1])
+    par = {'id': str(emp_id)}
+    r = requests.get('https://jsonplaceholder.typicode.com/users', params=par)
 
-# Get employee name
-emp_id = int(argv[1])
-par = {'id': str(emp_id)}
-r = requests.get('https://jsonplaceholder.typicode.com/users', params=par)
+    for dic in r.json():
+        if dic.get('id') == emp_id:
+            emp_name = dic.get('name')
+            username = dic.get('username')
 
-for dic in r.json():
-    if dic.get('id') == emp_id:
-        emp_name = dic.get('name')
-        username = dic.get('username')
-
-# Get employee's tasks
-par = {'userId': str(emp_id)}
-r = requests.get('https://jsonplaceholder.typicode.com/todos', params=par)
-done = []
-tasks = r.json()
-total = len(tasks)
-for task in tasks:
-    if task.get('completed') is True:
-        done.append(task.get('title'))
-
-# Print in correct format
-print("Employee {} is done with tasks ({}/{}):".format(
-      emp_name, len(done), total))
-for task in done:
-    print("\t {}".format(task))
-
-# Export to CSV
-filename = argv[1] + '.csv'
-with open(filename, mode='w') as f:
-    f = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    # Get employee's tasks
+    par = {'userId': str(emp_id)}
+    r = requests.get('https://jsonplaceholder.typicode.com/todos', params=par)
+    done = []
+    tasks = r.json()
+    total = len(tasks)
     for task in tasks:
-        status = task.get('completed')
-        title = task.get('title')
-        f.writerow([emp_id, username, status, title])
+        if task.get('completed') is True:
+            done.append(task.get('title'))
+
+    # Print in correct format
+    print("Employee {} is done with tasks ({}/{}):".format(
+        emp_name, len(done), total))
+    for task in done:
+        print("\t {}".format(task))
+
+    # Export to CSV
+    filename = argv[1] + '.csv'
+    with open(filename, mode='w') as f:
+        f = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            status = task.get('completed')
+            title = task.get('title')
+            f.writerow([emp_id, username, status, title])
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,49 +1,50 @@
 #!/usr/bin/python3
 """This module makes a request to a RESTful API."""
-import requests
 from sys import argv
 import json
+import requests
 
 
-if len(argv) < 2:
-    exit
+if __name__ == "__main__":
+    if len(argv) < 2:
+        exit
 
-# Get employee name
-employee_id = int(argv[1])
-par = {'id': str(employee_id)}
-r = requests.get('https://jsonplaceholder.typicode.com/users', params=par)
+    # Get employee name
+    employee_id = int(argv[1])
+    par = {'id': str(employee_id)}
+    r = requests.get('https://jsonplaceholder.typicode.com/users', params=par)
 
-for dic in r.json():
-    if dic.get('id') == employee_id:
-        employee_name = dic.get('name')
-        username = dic.get('username')
+    for dic in r.json():
+        if dic.get('id') == employee_id:
+            employee_name = dic.get('name')
+            username = dic.get('username')
 
-# Get employee's tasks
-par = {'userId': str(employee_id)}
-r = requests.get('https://jsonplaceholder.typicode.com/todos', params=par)
-done = []
-tasks = r.json()
-total = len(tasks)
-for task in tasks:
-    if task.get('completed') is True:
-        done.append(task.get('title'))
+    # Get employee's tasks
+    par = {'userId': str(employee_id)}
+    r = requests.get('https://jsonplaceholder.typicode.com/todos', params=par)
+    done = []
+    tasks = r.json()
+    total = len(tasks)
+    for task in tasks:
+        if task.get('completed') is True:
+            done.append(task.get('title'))
 
-# Print in correct format
-print("Employee {} is done with tasks ({}/{}):".format(
-      employee_name, len(done), total))
-for task in done:
-    print("\t {}".format(task))
+    # Print in correct format
+    print("Employee {} is done with tasks ({}/{}):".format(
+        employee_name, len(done), total))
+    for task in done:
+        print("\t {}".format(task))
 
-# Serialize to JSON file
-filename = argv[1] + '.json'
-task_list = []
-for task in tasks:
-    new_task = {}
-    new_task["task"] = task.get('title')
-    new_task["completed"] = task.get('completed')
-    new_task["username"] = username
-    task_list.append(new_task)
-j_obj = {employee_id: task_list}
+    # Serialize to JSON file
+    filename = argv[1] + '.json'
+    task_list = []
+    for task in tasks:
+        new_task = {}
+        new_task["task"] = task.get('title')
+        new_task["completed"] = task.get('completed')
+        new_task["username"] = username
+        task_list.append(new_task)
+    j_obj = {employee_id: task_list}
 
-with open(filename, "w") as f:
-    json.dump(j_obj, f)
+    with open(filename, "w") as f:
+        json.dump(j_obj, f)
